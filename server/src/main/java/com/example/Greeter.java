c84bb91161c37a01be8f3bded6e385db5e415443
jeremy-limx <32630695+jeremy-limx@users.noreply.github.com>
2017-10-16 14:45:39 +0800

    Fixed up errors flagged by checksum

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * This is to return a string.
+   */
+  
+  public final String greet(String someone) {
     return String.format("Hello, %s! Webhook Testing Again!", someone);
   }
 }
